bccb322f04ecc1aab2ec85049e2df67e57e683bd
Kalito Wilk <kalitovieiradasilva11@outlook.com>
2025-05-26 22:40:02 -0300

    modificacoes feitas em estilos e estrutura html

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,25 @@
 const btnClick = document.getElementById("btn");
 
+/*const inputs = document.getElementsByTagName("input");
+
+const usuario = inputs[0];
+
+const senha = inputs[1];*/
+
 btnClick.addEventListener('click', function () {
     alert("Desculpe! o sistema ainda está em desenvolvimento.")
 
 });
+
+/*btnClick.addEventListener('click', function() {
+
+    if(usuario.value == '' || senha.value === '') {
+
+        return alert('Por favor! preencha os campos de usuário e senha, obrigado!');
+    }
+
+    const mensagem = (`Seu usuário é: ${usuario.value} \nSua senha é: ${senha.value}.`);
+
+    alert(mensagem);
+
+});*/
